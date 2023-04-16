38e62d1a9cd7108d0a211a033bd9544263867715
NaimBiswas <nayeembiswas2@gmail.com>
2023-04-16 16:23:49 +0600

    formatted

--- src/controllers/categories/category.controller.js
+++ src/controllers/categories/category.controller.js
@@ -1,29 +1,29 @@
-const httpStatus = require("http-status")
-const categoryService = require("../../services/category/category.service")
+const httpStatus = require('http-status');
+const categoryService = require('../../services/category/category.service');
 
-const createCategory  = async (req, res) => {
-    req.attachUser.create()
-    const createCategory = await categoryService.createCategory(req,res)
+const createCategory = async (req, res) => {
+  req.attachUser.create();
+  const createCategory = await categoryService.createCategory(req, res);
 
-    if(createCategory) return res.status(httpStatus.CREATED).json({success:true, data: createCategory})
-}
+  if (createCategory) return res.status(httpStatus.CREATED).json({ success: true, data: createCategory });
+};
 
 const getAllCategory = async (req, res) => {
-    const cat = await categoryService.getAllCategory(req, res)
-    if(cat) return res.status(httpStatus.OK).json({success:true, data: cat})
-}
+  const cat = await categoryService.getAllCategory(req, res);
+  if (cat) return res.status(httpStatus.OK).json({ success: true, data: cat });
+};
 
 const getCategoryById = async (req, res) => {
-    const cat = await categoryService.getCategoryById(req, res)
-    if(cat) return res.status(httpStatus.OK).json({success:true, data: cat})
-}
+  const cat = await categoryService.getCategoryById(req, res);
+  if (cat) return res.status(httpStatus.OK).json({ success: true, data: cat });
+};
 const updateCategory = async (req, res) => {
-    const cat = await categoryService.updateCategory(req, res)
-    if(cat) return res.status(httpStatus.OK).json({success:true,message:"Category updated successfully", data: cat})
-}
+  const cat = await categoryService.updateCategory(req, res);
+  if (cat) return res.status(httpStatus.OK).json({ success: true, message: 'Category updated successfully', data: cat });
+};
 const deleteCategory = async (req, res) => {
-    const cat = await categoryService.deleteCategory(req, res)
-    if(cat) return res.status(httpStatus.OK).json({success:true,message:"Category deleted successfully", data: cat})
-}
+  const cat = await categoryService.deleteCategory(req, res);
+  if (cat) return res.status(httpStatus.OK).json({ success: true, message: 'Category deleted successfully', data: cat });
+};
 
-module.exports = {createCategory, getAllCategory,getCategoryById,updateCategory, deleteCategory}
+module.exports = { createCategory, getAllCategory, getCategoryById, updateCategory, deleteCategory };
